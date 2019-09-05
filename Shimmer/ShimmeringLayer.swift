a804f9f37ac5c491ecca4abc48d69a6c0a8f94f3
Zene Sekou <zene.m.sekou@disney.com>
2019-09-05 16:54:47 -0700

    change direction

--- Shimmer/ShimmeringLayer.swift
+++ Shimmer/ShimmeringLayer.swift
@@ -195,7 +195,7 @@ final public class ShimmeringLayer: CALayer {
             maskLayer?.position = CGPoint(x: 0, y: -travelDistance)
             maskLayer?.bounds = CGRect(x: 0, y: 0, width: content.bounds.width, height: fullShimmerLength)
         } else if shimmerDirection == .bottomLeftToTopRight {
-          maskLayer?.startPoint = CGPoint(x: 0 y: 0)
+          maskLayer?.startPoint = CGPoint(x: 0, y: 0)
           maskLayer?.endPoint = CGPoint(x: 1, y: 1)
           maskLayer?.position = CGPoint(x: 0, y: -travelDistance)
           maskLayer?.bounds = CGRect(x: 0, y: 0, width: content.bounds.width, height: fullShimmerLength)
